8791aa7c24f31d1d4acd3ac076c771b46e7f0ba1
ashwinii19 <ashdagale@gmail.com>
2026-05-13 12:55:52 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/LibraryMgmtBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgmtBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMgmtBeApplication.class, args);
+	}
+
+}
